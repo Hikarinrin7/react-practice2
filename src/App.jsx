72dbe6c528fada38262081dc3dcef5c4f264f666
Hikarinrin7 <shizuoka_iitoko@yahoo.co.jp>
2025-01-31 12:01:50 +0900

    イベントとスタイルの扱い方まで

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@
 // returnの中身は必ずタグで囲む
 // ファイルを分けたら、export付けることでこの関数を他でも使えるようになる
 // importはmain側で使うときに自動で追加される
+
+
 export const App = () => {
+    // return内での操作はjsの関数として{}で、スタイルはjsのオブジェクトとして
+    const onClickButton = () => alert();
+    const contentStyle = {
+        color: "blue",
+        fontSize: "18px",
+    }
+
     return (
         <div>
             <h1>こんにちは！</h1>
-            <p>お元気ですか？</p>
+            <p style={contentStyle}>お元気ですか？</p>
+            <button onClick={onClickButton}>ボタン</button>
         </div>
     );
 };
