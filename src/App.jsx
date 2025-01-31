76d2b77099c5dbdffeace32548c3f1a9530c1ad5
Hikarinrin7 <shizuoka_iitoko@yahoo.co.jp>
2025-01-31 15:00:39 +0900

    stateまで

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// use~という関数（フックと呼ばれる）
+import { useState } from "react";
+
 // reactでは、画面の要素はすべて関数（コンポーネント）で書く！！
 // returnの中身は必ずタグで囲む
 // ファイルを分けたら、export付けることでこの関数を他でも使えるようになる
@@ -7,15 +10,24 @@ import { ColorfulMessage } from "./components/ColorfulMessage";
 
 
 export const App = () => {
+    // この値はstateとして定義しますよ〜と宣言
+    // useState関数は、「実際のstate」と「stateを更新するための関数」の二つを受け取る
+    // 配列の分割代入で受け取る
+    // stateの初期値は0にしてある
+    const [num, setNum] = useState(0);
     // return内でのイベントはjsの関数として{}で、スタイルはjsのオブジェクトとして
-    const onClickButton = () => alert();
+    const onClickCountUp = () => {
+        setNum(num + 1);
+        setNum((prev)=>prev+1);
+    };
 
     return (
         <div>
             <h1>こんにちは！</h1>
             <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
             <ColorfulMessage color="green">元気です！</ColorfulMessage>
-            <button onClick={onClickButton}>ボタン</button>
+            <button onClick={onClickCountUp}>カウントアップ</button>
+            <p>{num}</p>
         </div>
     );
 };
